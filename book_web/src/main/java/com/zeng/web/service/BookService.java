6d8d83e091ebdce07fdc775e70dc7adf67f32c0c
zengchao <uljaxsec1991@163.com>
2016-10-06 16:57:10 +0800

    init by zengchao

---
+++ book_web/src/main/java/com/zeng/web/service/BookService.java
@@ -0,0 +1,9 @@
+package com.zeng.web.service;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public interface BookService {
+
+    public void findAll();
+}
---
+++ book_web/src/main/java/com/zeng/web/service/impl/BookServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zeng.web.service.impl;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class BookServiceImpl implements BookService {
+
+    @Override
+    public void findAll() {
+
+    }
+}
